2f75db69b3ee7d88d12a170a1cd4f676221f1253
Christopher Stephenson <hpufo2@gmail.com>
2017-01-29 23:01:07 -0500

    initial commit

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+    private Transform thisTransform;        //Cached transform for this projectile
+
+	// Use this for initialization
+	void Start () {
+        thisTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        thisTransform.position += Time.deltaTime * speed * thisTransform.forward;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
